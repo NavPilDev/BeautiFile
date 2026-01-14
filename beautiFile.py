46ce4e7c54e17fbdf02f2c9b7319ae66ee65390f
NavPilDev <abhinavpillai04@gmail.com>
2026-01-14 02:10:10 -0600

    Fixed Add FileIconSearch

--- beautiFile.py
+++ beautiFile.py
@@ -1,17 +1,56 @@
 import sys
 import subprocess
 from PySide6.QtWidgets import (
-    QApplication, QWidget, QFrame,
-    QToolButton, QGridLayout
+    QApplication, QMainWindow, QWidget, QFrame,
+    QToolButton, QGridLayout, QFileIconProvider
 )
 from PySide6.QtGui import QIcon
-from PySide6.QtCore import Qt, QSize
+from PySide6.QtCore import Qt, QSize, QFileInfo
+import win32com.client
 
 APPS = [
-    ("Valorant", "icons/valorant.png", r"C:\Riot Games\Riot Client\RiotClientServices.exe"),
-    ("Steam", "icons/steam.png", r"C:\Program Files (x86)\Steam\Steam.exe"),
-    ("Epic Games", "icons/epic.png", r"C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win64\EpicGamesLauncher.exe"),
-    ("Marvel Rivals", "icons/marvel.png", r"C:\Games\MarvelRivals.exe"),
+    ("Valorant", r"C:\Riot Games\Riot Client\RiotClientServices.exe"),
+    ("Steam", r"C:\Program Files (x86)\Steam\Steam.exe"),
+    ("Epic Games", r"C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win64\EpicGamesLauncher.exe"),
+    ("Marvel Rivals", r"C:\Games\MarvelRivals.exe"),
 ]
 
+class MainWindow(QWidget) :
+    def __init__(self):
+        super().__init__()
+        
+        icon_provider = QFileIconProvider()
 
+        container = QFrame(self)
+        container.setStyleSheet("""
+        QFrame {
+            background-color: rgba(36, 36, 36, 210);
+        }
+        """)
+        layout = QGridLayout(container)
+        layout.setContentsMargins(20, 20, 20, 20)
+        
+        for name, path in APPS:
+            btn = QToolButton()
+            btn.setText(name)
+
+            file_info = QFileInfo(path)
+            icon = icon_provider.icon(file_info)
+            btn.setIcon(QIcon(icon))
+
+            btn.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+            btn.setIconSize(QSize(64, 64))
+            
+            btn.clicked.connect(lambda _, p = path: subprocess.Popen(p))
+            layout.addWidget(btn)
+        
+        self.resize(container.sizeHint())
+        container.resize(self.size())
+
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+app.exec()
